732630fd2a93768df0b09b451d91da0759041506
Eliseo Murillo <eliseomurillo9@gmail.com>
2022-04-06 11:42:12 +0200

    responsive and scroll

--- src/jsva.js
+++ src/jsva.js
@@ -1,15 +1,36 @@
 const button = document.getElementById('chat-button');
 const chat = document.getElementById('chatwindow');
 const closePop = document.getElementById('close');
+const nav = document.querySelector('#navs');
+const naves = nav.querySelectorAll('div.nav-links > a')
 
 
 
-button.addEventListener('click', function (event) {
+button.addEventListener('click', function (_event) {
     chat.classList.add('chat')
 });
 
-closePop.addEventListener('click', function (event) {
+closePop.addEventListener('click', function (_event) {
     chat.classList.remove('chat')
+});
+
+
+naves.forEach(a => {
+    a.addEventListener('click', function (e) {
+
+        if (this.hash !== '') {
+            e.preventDefault
+        };
+         const hash = this.hash
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+
+    })
 })
+
+
+
 
 
