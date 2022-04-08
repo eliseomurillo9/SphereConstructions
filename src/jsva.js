631939fad1585bec18d1377c9a9a8f70a220c35b
Eliseo Murillo <eliseomurillo9@gmail.com>
2022-04-08 22:10:48 +0200

    git

--- src/jsva.js
+++ src/jsva.js
@@ -2,17 +2,25 @@ const button = document.getElementById('chat-button');
 const chat = document.getElementById('chatwindow');
 const closePop = document.getElementById('close');
 const nav = document.querySelector('#navs');
-const naves = nav.querySelectorAll('div.nav-links > a')
+const naves = nav.querySelectorAll('div.nav-links > a');
+const options = document.querySelector('.nav-links')
+const menu = document.getElementById('menu');
 
 
 
 button.addEventListener('click', function (_event) {
     chat.classList.add('chat')
+    button.style.display = 'none'
 });
 
 closePop.addEventListener('click', function (_event) {
     chat.classList.remove('chat')
+    button.style.display = 'flex'
 });
+
+menu.addEventListener('click', function(e){
+    options.style.display = 'flex'
+})
 
 
 naves.forEach(a => {
@@ -26,6 +34,8 @@ naves.forEach(a => {
         $('html, body').animate({
             scrollTop: $(hash).offset().top
         }, 800);
+
+        options.style.display = 'none'
 
     })
 })
